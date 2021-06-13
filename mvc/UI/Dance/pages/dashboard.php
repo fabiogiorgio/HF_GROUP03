3ba1db27fa39264cafb25bdd045b4ee3fea65b04
phatle281200 <tple281200@gmail.com>
2021-06-13 13:16:08 +0200

    revert changes

--- mvc/UI/Dance/pages/dashboard.php
+++
@@ -1,4 +0,0 @@
-<input type="hidden" id="order" value='<?= json_encode($data['order']) ?>' />
-<div id="page-wrapper">
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-</div>
--- mvc/UI/admin.php
+++
@@ -1,5 +0,0 @@
-<?php require_once "./mvc/views/pages/index.php" ?>
-<?php require_once "./mvc/views/pages/header.php" ?>
-<?php require_once "./mvc/views/pages/menu.php" ?>
-<?php require_once "./mvc/views/pages/".$data["page"].".php" ?>
-<?php require_once "./mvc/views/pages/footer.php" ?>
